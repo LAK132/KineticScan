2244b056fd6e21fb77a2318e63cdea212076236f
LAK132 <lak132@github.com>
2020-06-23 20:13:20 +0930

    Fix compiling when LIBDIR isn't set

--- makelist.bat
+++ makelist.bat
@@ -1,13 +1,16 @@
 set BINDIR=bin
 set OBJDIR=obj
 
-set INCDIRS=include include\SDL include\imgui include\imgui\misc\cpp "%KINECTSDK10_DIR%inc"
-set LIBDIR=lib
+REM set INCDIRS=include "%KINECTSDK10_DIR%inc" include\SDL include\imgui include\imgui\misc\cpp
+REM set LIBDIR=lib
 REM set LIBS=SDL2main.lib SDL2.lib
 REM Global libs
 REM set GLIBS="%KINECTSDK10_DIR%lib\amd64\kinect10.lib"
+REM set SOURCE=src\main.cpp
 
+set INCDIRS=include "%KINECTSDK10_DIR%inc"
 set SOURCE=src\nogui.cpp
+
 set BINARY=kinetic.exe
 
 set CXX=cl /nologo /std:c++17 /D_CRT_SECURE_NO_WARNINGS /MT /EHsc
